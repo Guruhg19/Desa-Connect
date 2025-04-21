5daf4e9f32d254f5eec4bd7a0dac0e65eefa2b4a
Guruhg19 <guruhvitra19@gmail.com>
2025-04-21 19:49:03 +0700

    Add SocialAssistanceRecipientFactory and Seeder; update SocialAssistanceRecipient model with HasFactory trait and add foreign key relationships in migration.

--- database/migrations/2025_04_14_144712_create_social_assistance_recipients_table.php
+++ database/migrations/2025_04_14_144712_create_social_assistance_recipients_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->uuid('social_assistance_id');
             $table->foreign('social_assistance_id')->references('id')->on('social_assistances');
 
+            $table->uuid('head_of_family_id');
+            $table->foreign('head_of_family_id')->references('id')->on('head_of_families');
+
             $table->enum('bank',['bri','bca','bni','mandiri']);
             $table->decimal('amount');
             $table->longText('reason');
