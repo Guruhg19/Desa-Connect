aab4e3532ab63a496b3143d76dd01efc859a3959
Guruhg19 <guruhvitra19@gmail.com>
2025-04-25 13:28:18 +0700

    Add DevelopmentFactory and DevelopmentSeeder; implement seeding for Development model with sample data.

---
+++ database/seeders/DevelopmentSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Database\Factories\DevelopmentFactory;
+use Database\Factories\EventFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DevelopmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DevelopmentFactory::new()->count(10)->create();
+    }
+}
